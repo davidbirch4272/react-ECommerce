4f7ddf9e6a470e116fd9d4a6ac8a3f84aabf5abe
davidbirch4272 <davidbirch4272@gmail.com>
2025-04-25 11:50:08 -0400

    changes

--- src/components/ui/Book.jsx
+++ src/components/ui/Book.jsx
@@ -1,42 +1,59 @@
-import React from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { Link } from "react-router-dom";
 import Rating from "../ui/Rating";
 import Price from "./Price";
 
 const Book = ({ book }) => {
-  function imageLoaded() { 
-    console.log('imageLoaded')
-  }
+  const [img, setImg] = useState();
+
+  const mountedRef = useRef(true)
   
-  return (
+  useEffect(() => {
+    const image = new Image();
+    image.src = book.url;
+    image.onload = () => {
+      setTimeout (() => {
+        if (mountedRef.current) {
+        setImg(image);
+        }
+     }, 300);
+    };
+    return () => {
+      mountedRef.current = false;
+    }
+  });
+
+  return(
+
     <div className="book">
-    {
-      img ? 
-      <>
-      <Link to={`/books/${book.id}`}>
-        <figure className="book__image--wrapper">
-          <img src={book.url} alt="" className="book__img" onLoad={imageLoaded} />
-        </figure>
-      </Link>
-      <div className="book__title">
-        <Link to={`/books/${book.id}`} className="book__title--link">
-          {book.title}
-        </Link>
-      </div>
-      <Rating rating={book.rating} />
-      <Price salePrice={book.salePrice} originalPrice={book.originalPrice} />
-        />
+      {img ? (
+        <>
+          <Link to={`/books/${book.id}`}>
+            <figure className="book__image--wrapper">
+              <img src={img.src} alt="" className="book__img" />
+            </figure>
+          </Link>
+          <div className="book__title">
+            <Link to={`/books/${book.id}`} className="book__title--link">
+              {book.title}
+            </Link>
+          </div>
+          <Rating rating={book.rating} />
+          <Price
+            salePrice={book.salePrice}
+            originalPrice={book.originalPrice}
+          />
         </>
-       ) :  (
-    <></>
-    )}
-  
-  <div className="book__img--skeleton"></div>
-  <div className="skeleton book__title--skeleton"></div>
-  <div className="skeleton book__rating--skeleton"></div>
-  <div className="skeleton book__price--skeleton"></div>
-  </div>
-      );
+      ) : (
+        <>
+          <div className="book__img--skeleton"></div>
+          <div className="skeleton book__title--skeleton"></div>
+          <div class Name="skeleton book__rating--skeleton"></div>
+          <div className="skeleton book__price--skeleton"></div>
+        </>
+      )}
+    </div>
+  );
 };
 
 export default Book;
